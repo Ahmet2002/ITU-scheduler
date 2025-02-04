546314135db63b84ba1b3d24b87e51a2f759b07b
Ahmet2002 <keklika20@itu.edu.tr>
2025-02-04 21:06:56 +0300

    fixed a bug in update_database

--- database_update/update_database.py
+++ database_update/update_database.py
@@ -1,11 +1,5 @@
 from bs4 import BeautifulSoup
 import sqlite3, json, re, logging, requests
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import time
 
 
 class CourseScraper:
